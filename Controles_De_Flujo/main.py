fa11b6de138a6289a1f7167dfc6f288a10218d2e
moisespenafiel10 <moipenafieltello@gmail.com>
2023-06-27 10:03:19 -0500

    nueva clase

--- Controles_De_Flujo/main.py
+++ Controles_De_Flujo/main.py
@@ -9,11 +9,28 @@
 #for vocal in vocales:
 #  print(vocal)
 
-colores = ['amarillo','rojo','azul','marron','anaranjado',]
-clr = input('ingrese color : ')
-for color in colores:
-  if color == clr :
-    print(color)
-    print(f"Se encontro el color:{color}")
-    break
-  print(color)
+# colores = ['amarillo','rojo','azul','marron','anaranjado',]
+# clr = input('ingrese color : ')
+# for color in colores:
+#   if color == clr :
+#     print(color)
+#     print(f"Se encontro el color:{color}")
+#     break
+#   print(color)
+
+# dato = int(input('ingrese numero : '))
+ 
+# for numero in range(1,12):
+#     multiplicacion = dato * numero
+#     print(f"""{dato}*{numero} = {multiplicacion} """)
+
+dato = int(input('ingrese numero : '))
+factorial = 1
+if dato == 0 :
+  print('el factorial de 0 es 0')
+else:
+  for numero in range(1,dato + 1):
+    factorial = factorial*numero
+    print(factorial)
+
+    
